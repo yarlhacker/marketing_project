dd9404bbf2aa7d16361ea8a8058a82674119e731
yarlhacker <ismael.developper@gmail.com>
2021-06-01 12:31:37 +0000

    commit

--- marketing_project/settings.py
+++ marketing_project/settings.py
@@ -127,12 +127,12 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = BASE_DIR / '/static/'
+STATIC_ROOT = BASE_DIR / '/static_cdn/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media_cdn'
 
+STATICFILES_DIRS =  [ BASE_DIR / 'static']
 
-STATICFILES_DIRS =  [ BASE_DIR , 'static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
